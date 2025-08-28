49e0e795703cf9cbad004e875e2d5a9f9289b848
danilopereranunis <00001099492841sp@al.educacao.sp.gov.br>
2025-08-28 14:27:49 -0300

    Criamos funções no javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,48 @@
 const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
+const modalLogin = document.querySelector("#modal-login");
+const modalRegister = document.querySelector("#modal-register");
+const btnCancelLogin = document.querySelector("#btn-cancel-login");
+const btnCancelRegister = document.querySelector("#btn-cancel-register")
 
+function soma(num01, num02) {
+  const somar = num01 + num02;
+  console.log(somar);  
+}
+
+soma(8, 6)
+
+// arrow function
+const multiplicar = (num01, num02) => {
+  return num01 * num02
+}
+
+console.log(miltiplicar(5, 8,9));
+
+const subitrair = (num01, num02) => {
+  return num01 - num02
+}
+
+console.log(subitrair(4, 9-4));
+
+
+// addEventlisner => Escuta o evento de click e executa a função
 btnLogin.addEventListener("click", (e) => {
-    console.log("Cliquei no butão login");
-    
+  modalLogin.classList.remove("hide-modal");
+  modalLogin.classList.add("show-modal");
+})
+
+btnRegister.addEventListener("click", (e) => {
+  modalRegister.classList.remove("hide-modal");
+  modalRegister.classList.add("show-modal");
+})
+
+btnCancelLogin.addEventListener("click", (e) => {
+  modalLogin.classList.remove("show-modal");
+  modalLogin.classList.add("hide-modal");
+})
+
+btnCancelRegister.addEventListener("click", (e) => {
+  modalRegister.classList.remove("show-modal");
+  modalRegister.classList.add("hide-modal");
 })
